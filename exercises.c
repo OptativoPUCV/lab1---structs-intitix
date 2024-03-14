66c93324d0bd27afd4a00b0d2ba6afbf0bb04493
Your Name <you@example.com>
2024-03-14 20:39:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -1,22 +1,36 @@
-a #include <ctype.h>
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 
 /*
 Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size) { return 0; }
+int findMax(int arr[], int size)
+{
+  int mayor = arr[0];
+  for (int i = 0; i < size; i++)
+    {
+      if (arr[i] > arr[i + 1])
+        mayor = arr[i];
+    }
+  return mayor;
+}
+
 
 /*
 Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-void reverseArray(int arr[], int size) {}
+void reverseArray(int arr[], int size) {
+  
+}
 
 /*
 Ejercicio 3: Filtrar Números Pares
