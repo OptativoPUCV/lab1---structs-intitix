5775f0a54288273ae92bba5085ba150df1813d57
Your Name <you@example.com>
2024-03-14 20:40:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -16,7 +16,7 @@ int findMax(int arr[], int size)
   int mayor = arr[0];
   for (int i = 0; i < size; i++)
     {
-      if (arr[i] > arr[i + 1])
+      if (arr[i] > mayor)
         mayor = arr[i];
     }
   return mayor;
